4db3fff276726fe3ae0058932f4bd4125f6bf7ee
Hansel <valkirecesa@gmail.com>
2021-11-25 23:39:29 +0800

    matrixToList: added convert MatrixToList function

--- matrixToList/index.c
+++ matrixToList/index.c
@@ -29,11 +29,15 @@ typedef int MATRIX[MAX_VERTEX][MAX_VERTEX]; // static matrix
 
 void displayMatrix(MATRIX M)
 {
+    printf("%3c", ' ');
+    for (int i = 0; i < MAX_VERTEX; i++)
+        printf("%3d ", i);
+
+
 	for (int i = 0; i < MAX_VERTEX; i++) {
 		printf("\n%d: ", i);
-		for (int j = 0; j < MAX_VERTEX; j++) {
+		for (int j = 0; j < MAX_VERTEX; j++)
 			M[i][j] == INF ? printf("%3c ", 236) : printf("%3d ", M[i][j]);
-		}
 		printf("\n");
 	}
 }
@@ -41,8 +45,48 @@ void displayMatrix(MATRIX M)
 AdjList convertMatrixToList(MATRIX M)
 {
     AdjList adj_list;
-    adj_list = (AdjList) calloc (MAX_VERTEX, sizeof(node));
+    adj_list = (AdjList) calloc (MAX_VERTEX, sizeof(nodePtr));
+    if (adj_list) {
+        
+        // loop matrix to get (u, v)
+        for (int u = 0; u < MAX_VERTEX; u++) {
+            for (int v = 0; v < MAX_VERTEX; v++) {
+                
+                // if there is an edge,
+                // we add that to the adj list
+                // M[u][v] is also the weight.
+                if (M[u][v] != INF) {
+
+                    // traverse until the end of u
+                    nodePtr *trav;
+                    trav = &adj_list[u];
+                    while (*trav) trav = &(*trav)->next;
+
+                    *trav = (nodePtr) calloc (1, sizeof(node));
+                    if (*trav) {
+                        (*trav)->adj_vertex = v;
+                        (*trav)->weight = M[u][v];
+                    }
+                }
+            }
+        }
+    }
     return adj_list;
+}
+
+void displayAdjList(AdjList adj_list)
+{
+    printf("\nadj_vertex(weight)");
+    for (int i = 0; i < MAX_VERTEX; i++) {
+
+        printf("\n%d: ", i);
+        nodePtr trav;
+        trav = adj_list[i];
+        while (trav) {
+            printf("%d(%d) ", trav->adj_vertex, trav->weight);
+            trav = trav->next;
+        }
+    }
 }
 
 int main()
@@ -59,6 +103,24 @@ int main()
 
     AdjList adj_list;
     adj_list = convertMatrixToList(M);
-    display(adj_list);
+    displayAdjList(adj_list);
+
+    /** 
+				Expected Output for Matrix
+				{INF,  10, INF, 30, 100},
+				{INF, INF,  50, INF, INF},
+				{ 20, INF, INF, INF, 10},
+				{INF, INF,  20, INF, 60},
+				{INF, INF, INF, INF, INF}
+                
+                =----------------------------=
+
+                Expected Output for List
+				0 -> 1(10) -> 3(30) -> 4(100)
+                1 -> 2(50)
+                2 -> 0(20) -> 4(10)
+                3 -> 2(20) -> 4(60)
+                4
+	**/
 }
 
