fb1237d95745d5c01ecc8e668be9664aebce5427
Hansel <valkirecesa@gmail.com>
2021-11-25 22:56:00 +0800

    givenEdgeToMatrix: added display and createMatrix function

--- givenEdgeToMatrix/index.c
+++ givenEdgeToMatrix/index.c
@@ -9,3 +9,82 @@
 
 **/
 
+#define MAX_VERTEX 5
+#define MAX 10 // n(n - 1)/2 yields 10
+
+/* An unweighted, undirected Edge */
+typedef struct {
+	int vertex;
+	int adj_vertex;
+}edgeType;
+
+typedef struct {
+	edgeType Edge[MAX];
+	int edge_count;
+}EdgeList;
+
+typedef int MATRIX_1[MAX_VERTEX][MAX_VERTEX]; // static
+typedef int ** MATRIX; // dynamic
+
+
+// MATRIX func_name
+int ** createAdjacencyMatrix(EdgeList E)
+{
+    int ** MATRIX;
+    // each element has to be an array.
+    MATRIX = (int **) calloc (MAX_VERTEX, sizeof(int *));
+
+    // initialize each array element
+    for (int i = 0; i < MAX_VERTEX; i++) {
+        MATRIX[i] = (int *) calloc (MAX_VERTEX, sizeof(int));
+    }
+
+    for (int i = 0; i < E.edge_count; i++) {
+        
+        // get vertex and adjacency vertex
+        int u = E.Edge[i].vertex;
+        int v = E.Edge[i].adj_vertex;
+
+        // insert to [u][v] and [v][u]
+        MATRIX[u][v] = 1;
+        MATRIX[v][u] = 1;
+
+    }
+    return MATRIX;
+}
+                    // int ** M
+void displayMatrix(MATRIX M)
+{
+    printf("%3c", ' ');
+    for (int i = 0; i < MAX_VERTEX; i++)
+        printf("%d ", i);
+
+    for (int i = 0; i < MAX_VERTEX; i++) {
+        printf("\n%d: ", i);
+        for (int j = 0; j < MAX_VERTEX; j++)
+            printf("%d ", M[i][j]);
+    }
+}
+
+int main()
+{
+	EdgeList E = {
+					{{0,1},{0,2},{0,3},{0,4},
+						{1,2},{2,3},{2,4},{3,4}}, 
+					8
+					};
+	int ** M; // or MATRIX M;
+	
+	M = createAdjacencyMatrix(E);
+	displayMatrix(M);
+					
+	/** 
+				Expected Output
+				{0, 1, 1, 1, 1},
+				{1, 0, 1, 0, 0},
+				{1, 1, 0, 1, 1},
+				{1, 0, 1, 0, 1},
+				{1, 0, 1, 1, 0},
+	**/
+	return 0;
+}
