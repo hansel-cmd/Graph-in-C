aee727685737c2b5d84eff821239e57408bd8b54
Hansel <valkirecesa@gmail.com>
2021-11-26 11:27:40 +0800

    DFS Matrix Traversal: Todo

---
+++ DFSMatrixTraversal/index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// This is a DFS Traversal on a Matrix representation.
+// We use Recursive Approach on this file.
+
+// Your goal is to create the Iterative Approach of DFS on a Matrix.
+// See the solution.c inside DFSMatrixTraversal.
+
+// Todo
