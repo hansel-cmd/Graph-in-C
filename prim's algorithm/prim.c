3fe91e02d757a74f6a2f1e1708cf0e01bda394c9
Hansel <valkirecesa@gmail.com>
2021-12-03 22:22:37 +0800

    prim's algo base code

---
+++ prim's algorithm/prim.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "struct_definition.h"
+#include "helper.h"
+
+/**
+ *              This is Prim's algorithm. It is used for finding the Minimum Spanning Tree of a graph.
+ *                   This version of Prim's algorithm is similar to Dijkstra's native style. This algorithm
+ *                  can also be implemented using a minimum priority queue.
+ * 
+ * 
+ **/
+
+int main()
+{
+    return 0;
+}
